dd9e0b9a64a33dcdcace271bb0f213f20f32f0b1
praise-laurine <praiselaurine10@gmail.com>
2020-11-21 22:26:53 +0300

    add setUp method

--- user_test.py
+++ user_test.py
@@ -1,3 +1,15 @@
 import unittest
 from user import User
 from user import Credentials
+
+class TestUser(unittest.TestCase):
+    '''
+    Test class that defines test cases for the user class behaviours
+    '''
+    def setUp(self):
+        '''
+        Set Up method to run before each individual test case
+        '''
+        self.new_user = User("PraiseLaurine","Test435s")
+
+        
