45bfe444fc5ac8a2efb6cfb5c39276914a2db72f
praise-laurine <praiselaurine10@gmail.com>
2020-11-21 14:34:31 +0300

    add methods

--- user.py
+++ user.py
@@ -6,7 +6,7 @@ class User:
 
     user_list = []
 
-    # def __init__(self,user_name,password):
-    #     self.user_name=user_name
-    #     self.password=password
+    def __init__(self,user_name,password):
+        self.user_name=user_name
+        self.password=password
         
