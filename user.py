c4251b937d07b5b94669ea12a915b3c951210ea8
praise-laurine <praiselaurine10@gmail.com>
2020-11-21 14:45:21 +0300

    add save_user function

--- user.py
+++ user.py
@@ -10,3 +10,14 @@ class User:
         self.user_name=user_name
         self.password=password
         
+    def save_user(self):
+
+        '''
+        save_user method saves user objects into user_list
+        '''
+
+        User.user_list.append(self)
+
+
+
+
