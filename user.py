db31eab0ed303b87fbe23d0ab4685a719f0ca154
praise-laurine <praiselaurine10@gmail.com>
2020-11-21 14:33:00 +0300

    added an empty array

--- user.py
+++ user.py
@@ -4,7 +4,7 @@ class User:
     Class that generates new instances of users
     """
 
-    # user_list = []
+    user_list = []
 
     # def __init__(self,user_name,password):
     #     self.user_name=user_name
