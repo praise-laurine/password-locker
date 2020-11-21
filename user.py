29998e1867e74ef2e0548cedd57d92221ff29fd0
praise-laurine <praiselaurine10@gmail.com>
2020-11-21 14:31:56 +0300

    add class

---
+++ user.py
@@ -0,0 +1,12 @@
+class User:
+    
+    """
+    Class that generates new instances of users
+    """
+
+    # user_list = []
+
+    # def __init__(self,user_name,password):
+    #     self.user_name=user_name
+    #     self.password=password
+        
